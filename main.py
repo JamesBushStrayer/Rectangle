51813dd94913e07dba5e43b57a15fca514ee1862
JamesBushStrayer <139084557+JamesBushStrayer@users.noreply.github.com>
2023-12-17 15:24:56 +0000

    Initial commit
    First commit of Rectangle lab for CIS 261

--- main.py
+++ main.py
@@ -1,35 +1,44 @@
+# Week 8 - Rectangle Lab
+# CIS 261
+# James Alan Bush (SU200619708)
+
+
 class Rectangle:
+
   def __init__(self, width, height):
-      self.width = int(width)
-      self.height = int(height)
+    self.width = int(width)
+    self.height = int(height)
 
   def calculate_perimeter(self):
-      return 2 * (self.width + self.height)
+    return 2 * (self.width + self.height)
 
   def calculate_area(self):
-      return self.width * self.height
+    return self.width * self.height
 
   def draw(self):
-      for row in range(self.height):
-          if row == 0 or row == self.height - 1:
-              print('*' * self.width)
-          else:
-              print('*' + ' ' * (self.width - 2) + '*')
+    for row in range(self.height):
+      if row == 0 or row == self.height - 1:
+        print('*' * self.width)
+      else:
+        print('*' + ' ' * (self.width - 2) + '*')
 
   def display(self):
-      print("Rectangle Width: {}".format(self.width))
-      print("Rectangle Height: {}".format(self.height))
-      print("Area: {}".format(self.calculate_area()))
-      print("Perimeter: {}".format(self.calculate_perimeter()))
-      print("\nDrawing of the rectangle:")
-      self.draw()
+    print("Height: {}".format(self.height))
+    print("Width: {}".format(self.width))
+    print("Area: {}".format(self.calculate_area()))
+    print("Perimeter: {}".format(self.calculate_perimeter()))
+    print("\nDrawing of the rectangle:")
+    self.draw()
+
 
 def main():
-  width = float(input("Enter the width of the rectangle: "))
-  height = float(input("Enter the height of the rectangle: "))
+  print("Rectangle Calculator\n")
+  height = float(input("Height: "))
+  width = float(input("Widthx: "))
 
   rect = Rectangle(width, height)
   rect.display()
+
 
 if __name__ == "__main__":
   main()
